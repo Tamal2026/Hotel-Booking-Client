59e139d94dd71896bc591d35a555c5dbeed8fb93
Tamal1 <tamalkrishna2026@gmail.com>
2023-11-06 01:33:26 +0600

    On Auth State Added for USer logged in checking

--- src/Components/Provider/AuthProvider.jsx
+++ src/Components/Provider/AuthProvider.jsx
@@ -1,45 +1,48 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import app from "../../Firebase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
-const AuthProvider = ({children}) => {
 
-    const {user,setUser} = useState(null)
-  
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-const createUser = (email,password) =>{
-    
-    return createUserWithEmailAndPassword(auth,email,password)
-}
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-const signIn = (email,password)=>{
-   
-    return signInWithEmailAndPassword(auth,email,password)
-}
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return ()=>{
+        return unSubscribe();
+    }
 
+  }, []);
 
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-const authInfo = {
-    user,createUser,signIn
-}
+  const logOut = () => {
+    return signOut(auth);
+  };
 
+  const authInfo = {
+    user,
+    createUser,
+    signIn,
+    logOut,
+  };
 
-
-
-
-
-
-
-
-
-
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
